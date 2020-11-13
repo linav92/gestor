65e11f750bf9608c7844025b61396a91714f52e7
Lina Vallejo <linav92@gmail.com>
2020-11-13 10:24:56 -0300

    validates presence true name, description and states fields, create errors and success into create.html.erb and fixed enum into index.html.erb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,14 @@
 class Project < ApplicationRecord
-    validates :name, :description, presence: true
-    enum states: %i[Propuesta En_progreso Terminado] 
+    validates :name, :description, :states, presence: true
+    enum states: {Propuesta: 0,  En_progreso: 1, Terminado: 2} 
 
-    # before_save :has_states
-    # def has_states
-    #     if self.states.present?
-    #         self.states.save
-    #     else
-    #         self.states = "propuesta"
-    #     end
-    # end     
+
+    before_save :has_states
+    def has_states
+        if self.states.present?
+            self.states.save
+        else
+            self.states = "Propuesta"
+        end
+    end     
 end
